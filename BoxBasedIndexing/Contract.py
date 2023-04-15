be828db758293522f2c8e3813e8f0bc07fcf27f1
Ludovit Scholtz <ludko2@gmail.com>
2023-04-15 09:39:49 +0200

    search through multiple boxes

--- BoxBasedIndexing/Contract.py
+++ BoxBasedIndexing/Contract.py
@@ -30,6 +30,36 @@ def distance(x1, y1, x2, y2):
     # Return the distance
     return dist
 
+@Subroutine(TealType.none)
+def findAndLogResults(boxname, FindX, FindY, Radius, FindXMin, FindXMax, FindYMin, FindYMax):
+    FindContents = App.box_get(boxname)
+    MaxIndex = Btoi(Substring(FindContents.value(),Int(0),Int(8))) # first 8 bytes stores length of the list of items in box
+    MaxIndexScratch = ScratchVar(TealType.uint64)
+    i = ScratchVar(TealType.uint64)
+    storedXScratch = ScratchVar(TealType.uint64)
+    storedYScratch = ScratchVar(TealType.uint64)
+    storedX =Btoi(App.box_extract(boxname, i.load() * Int(60) + Int(8+12+3*8), Int(8)))
+    storedY =Btoi(App.box_extract(boxname, i.load() * Int(60) + Int(8+12+4*8), Int(8)))
+    storedObject =App.box_extract(boxname, i.load() * Int(60) + Int(8), Int(60))
+    logic = Seq([
+        FindContents,
+        If(FindContents.hasValue(),
+            Seq([
+                MaxIndexScratch.store(MaxIndex),
+                For(i.store(Int(0)), i.load() < MaxIndex, i.store(i.load() + Int(1))).Do(
+                    Seq([
+                        storedXScratch.store(storedX),
+                        storedYScratch.store(storedY),
+                        If(And(storedXScratch.load() >= FindXMin, storedXScratch.load() <= FindXMax, storedYScratch.load() >= FindYMin, storedYScratch.load() <= FindYMax), # use expensive sqrt only if it matches the box
+                            If(distance(FindX, FindY, storedXScratch.load(), storedYScratch.load()) <= Radius , Log(storedObject)),
+                        ),
+                    ])
+                ),
+            ]),
+        ),
+    ])
+    return logic
+
 def approval_program():
     is_creator = Txn.sender() == Global.creator_address()
 
@@ -76,32 +106,30 @@ def approval_program():
 
     FindX = Btoi(Txn.application_args[1])
     FindY = Btoi(Txn.application_args[2])
-    FindInBoxname = getBox(FindX,FindY)
-    FindContents = App.box_get(FindInBoxname)
-    MaxIndex = Btoi(Substring(FindContents.value(),Int(0),Int(8)))
     Radius = Btoi(Txn.application_args[3])
     FindXMin = If(FindX > Radius, FindX, Int(0))
     FindXMax = If(FindX + Radius < Int(10000), FindX + Radius, Int(10000))
     FindYMin = If(FindY > Radius, FindY, Int(0))
     FindYMax = If(FindY + Radius < Int(10000), FindY + Radius, Int(10000))
     i = ScratchVar(TealType.uint64)
-    storedX =Btoi(App.box_extract(FindInBoxname, i.load() * Int(60) + Int(8+12+3*8), Int(8)))
-    storedY =Btoi(App.box_extract(FindInBoxname, i.load() * Int(60) + Int(8+12+4*8), Int(8)))
-    storedObject =App.box_extract(FindInBoxname, i.load() * Int(60) + Int(8), Int(60))
     storedXScratch = ScratchVar(TealType.uint64)
     storedYScratch = ScratchVar(TealType.uint64)
+
+    FindInBoxnameTL = getBox(FindXMin,FindYMin)
+    FindInBoxnameTR = getBox(FindXMax,FindYMin)
+    FindInBoxnameBL = getBox(FindXMin,FindYMax)
+    FindInBoxnameBR = getBox(FindXMax,FindYMax)
+
     FindMonstersInLocation = Seq(
         [
-            FindContents,
-            For(i.store(Int(0)), i.load() < MaxIndex, i.store(i.load() + Int(1))).Do(
-                Seq([
-                    storedXScratch.store(storedX),
-                    storedYScratch.store(storedY),
-                    If(And(storedXScratch.load() >= FindXMin, storedXScratch.load() <= FindXMax, storedYScratch.load() >= FindYMin, storedYScratch.load() <= FindYMax), # use expensive sqrt only if it matches the box
-                        If(distance(FindX, FindY, storedXScratch.load(), storedYScratch.load()) <= Radius , Log(storedObject)),
-                    ),
-                ])
-            ),
+            # TOP LEFT BOX
+            findAndLogResults(FindInBoxnameTL, FindX, FindY, Radius, FindXMin, FindXMax, FindYMin, FindYMax),
+            # TOP RIGHT BOX
+            findAndLogResults(FindInBoxnameTR, FindX, FindY, Radius, FindXMin, FindXMax, FindYMin, FindYMax),
+            # BOTTOM LEFT BOX
+            findAndLogResults(FindInBoxnameBL, FindX, FindY, Radius, FindXMin, FindXMax, FindYMin, FindYMax),
+            # BOTTOM RIGHT BOX
+            findAndLogResults(FindInBoxnameBR, FindX, FindY, Radius, FindXMin, FindXMax, FindYMin, FindYMax),
             Return(Int(1)),
         ]
     )
